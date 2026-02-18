57fb014283f390a9d77f724710ac4a0486ce3973
Maksym Vovk <mvx.vovk@gmail.com>
2026-02-18 16:58:12 +0200

    feat: add date to archive name

--- tasks/archive.js
+++ tasks/archive.js
@@ -2,7 +2,8 @@ const zip = require('zip-local');
 const { paths } = require('./settings');
 
 function archive() {
-  zip.sync.zip(paths.build.main).compress().save(`${paths.build.main}/team.urenol-sng.lp0.zip`);
+  const currentDate = new Date().toISOString().split('T')[0];
+  zip.sync.zip(paths.build.main).compress().save(`${paths.build.main}/sketch-card-${currentDate}.zip`);
   return Promise.resolve("Done!");
 }
 
