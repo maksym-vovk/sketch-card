6ed3e6cce795ac1212ee1f8964f523b48e84e127
maksym-vovk <mvx.vovk@gmail.com>
2026-02-20 17:16:08 +0200

    feat: implement UniversalRenderer for dynamic presentation content and update thank you page structure

--- build/js/main.js
+++ build/js/main.js
@@ -434,12 +434,65 @@
 
     };
 
+    const UniversalRenderer = {
+      basePath: document.documentElement.lang === 'en' ? '' : '../',
+
+      _createElement(config) {
+        const element = document.createElement(config.tag); // Set class
+
+        if (config.className) {
+          element.className = config.className;
+        } // Set text content
+
+
+        if (config.text) {
+          element.textContent = config.text;
+        } // Set attributes
+
+
+        if (config.attributes) {
+          Object.entries(config.attributes).forEach(([key, value]) => {
+            // Handle image src with basePath
+            if (key === 'src' && !value.startsWith('http')) {
+              element.setAttribute(key, this.basePath + value);
+            } else {
+              element.setAttribute(key, value);
+            }
+          });
+        } // Append children
+
+
+        if (config.children) {
+          config.children.forEach(childConfig => {
+            const child = this._createElement(childConfig);
+
+            element.appendChild(child);
+          });
+        }
+
+        return element;
+      },
+
+      render(rootSelector, elements) {
+        const root = document.querySelector(rootSelector);
+        const fragment = document.createDocumentFragment();
+        elements.forEach(element => {
+          const createdElement = this._createElement(element);
+
+          fragment.appendChild(createdElement);
+        });
+        root.prepend(fragment);
+      }
+
+    };
+
     function main() {
       AppState.init();
       LanguageSwitcher.init();
       ScreenManager.init();
       FormsManager.init();
       NichesAccordion.init();
+      UniversalRenderer.render(".presentation-modal", JSON.parse(document.querySelector(".presentation-modal").dataset.config['weightControl'].elements));
     }
 
     main(); // if (document.documentElement.clientWidth < 480) {
@@ -454,4 +507,3 @@
     // }
 
 }());
-//# sourceMappingURL=main.js.map
