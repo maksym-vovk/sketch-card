fb5bcb17c4e35d8ddfeeda8ecaebe0660e581c07
maksym-vovk <mvx.vovk@gmail.com>
2026-04-14 19:10:41 +0300

    feat(reviews): integrate reviews slider and update dependencies

---
+++ src/js/modules/ReviewsSlider.js
@@ -0,0 +1,39 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+export const ReviewsSlider = {
+    instance: null,
+
+    init() {
+        const el = this._getSliderElement();
+        if (!el) return;
+
+        this.instance = this._createSwiper(el);
+    },
+
+    _getSliderElement() {
+        return document.querySelector('.reviews__slider');
+    },
+
+    _createSwiper(el) {
+        return new Swiper(el, {
+            modules: [Navigation],
+            slidesPerView: 1,
+            spaceBetween: 16,
+            speed: 450,
+            loop: false,
+
+            navigation: {
+                nextEl: '.reviews__nav-next',
+                prevEl: '.reviews__nav-prev',
+            },
+        });
+    },
+
+    destroy() {
+        if (this.instance) {
+            this.instance.destroy();
+            this.instance = null;
+        }
+    }
+};
