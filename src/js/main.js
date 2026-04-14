c62d76a109a7346b62004df51396b44efcec9d69
maksym-vovk <mvx.vovk@gmail.com>
2026-04-14 19:10:35 +0300

    feat(reviews): integrate reviews slider and update dependencies

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,12 @@ import {NichesAccordion} from "./modules/NichesAccordion";
 import {AppState} from "./modules/AppState";
 import {LanguageSwitcher} from "./modules/LanguageSwitcher";
 import {CourseSwitcher} from "./modules/CourseSwitcher";
+import {ReviewsSlider} from "./modules/ReviewsSlider";
 
 function main() {
   AppState.init()
   LanguageSwitcher.init()
+  ReviewsSlider.init();
   ScreenManager.init();
   FormsManager.init();
   NichesAccordion.init();
