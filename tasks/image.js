4b57958542b9b49b19c59de2beb0f77a68f41e9c
maksym-vovk <mvx.vovk@gmail.com>
2026-05-01 14:10:52 +0300

    feat(images): add image compression and WebP generation tasks

--- tasks/image.js
+++ tasks/image.js
@@ -1,11 +1,41 @@
-const { src, dest } = require('gulp');
-const { paths } = require('./settings');
-const browserSync = require('browser-sync');
+// const { src, dest } = require('gulp');
+// const { paths } = require('./settings');
+// const browserSync = require('browser-sync');
+//
+// function image() {
+//   return src(paths.src.img)
+//     .pipe(dest(paths.build.img))
+//     .pipe(browserSync.reload({ stream: true }));
+// }
+//
+// module.exports = image;
 
-function image() {
-  return src(paths.src.img)
-    .pipe(dest(paths.build.img))
-    .pipe(browserSync.reload({ stream: true }));
+const { src, dest } = require('gulp');
+const { paths, prod } = require('./settings');
+const browserSync = require('browser-sync');
+const gulpIf = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+const webp = require('gulp-webp');
+const { parallel } = require('gulp');
+
+function compressImages() {
+    return src(paths.src.img)
+        .pipe(gulpIf(prod(), imagemin([
+            mozjpeg({ quality: 80, progressive: true }),
+            pngquant({ quality: [0.6, 0.8] }),
+        ])))
+        .pipe(dest(paths.build.img))
+        .pipe(browserSync.reload({ stream: true }));
 }
+
+function generateWebp() {
+    return src('src/img/*.+(jpg|jpeg|png)')
+        .pipe(webp({ quality: 80 }))
+        .pipe(dest(paths.build.img));
+}
+
+const image = parallel(compressImages, generateWebp);
 
 module.exports = image;
